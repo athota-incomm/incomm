6a91535ce5ac6abc7ec26cfabb0863ca66706de7
athota-incomm <athota@incomm.com>
2017-10-05 16:28:20 +0530

    Incomm local project commit

---
+++ var/generation/Kensium/Testimonial/Controller/Adminhtml/Testimonial/MassDelete/Interceptor.php
@@ -0,0 +1,29 @@
+<?php
+namespace Kensium\Testimonial\Controller\Adminhtml\Testimonial\MassDelete;
+
+/**
+ * Interceptor class for @see \Kensium\Testimonial\Controller\Adminhtml\Testimonial\MassDelete
+ */
+class Interceptor extends \Kensium\Testimonial\Controller\Adminhtml\Testimonial\MassDelete implements \Magento\Framework\Interception\InterceptorInterface
+{
+    use \Magento\Framework\Interception\Interceptor;
+
+    public function __construct(\Magento\Backend\App\Action\Context $context, \Magento\Framework\Registry $registry)
+    {
+        $this->___init();
+        parent::__construct($context, $registry);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dispatch(\Magento\Framework\App\RequestInterface $request)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'dispatch');
+        if (!$pluginInfo) {
+            return parent::dispatch($request);
+        } else {
+            return $this->___callPlugins('dispatch', func_get_args(), $pluginInfo);
+        }
+    }
+}
